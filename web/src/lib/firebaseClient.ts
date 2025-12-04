9a72d7157e10133fbc058e433065dedb665b4238
skerishKang <skerish@naver.com>
2025-12-05 08:29:00 +0900

    chore: 웹 Firebase 클라이언트를 autohub-dev 프로젝트로 통일

--- web/src/lib/firebaseClient.ts
+++ web/src/lib/firebaseClient.ts
@@ -1,15 +1,15 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-// Firebase 웹 설정 (Firebase 콘솔에서 제공된 구성 값 사용)
+// Firebase 웹 설정 (Firebase 콘솔 autohub-dev 프로젝트의 autohub-web 앱 구성)
 const firebaseConfig = {
-  apiKey: "AIzaSyAOylHow4Bg8fk0O2FbiHcxA8wq6OPkRmo",
-  authDomain: "grantscout-af8da.firebaseapp.com",
-  projectId: "grantscout-af8da",
-  storageBucket: "grantscout-af8da.firebasestorage.app",
-  messagingSenderId: "790627865650",
-  appId: "1:790627865650:web:5fd14b09a61565bc8b582b",
-  measurementId: "G-CLWW6049WC",
+  apiKey: "AIzaSyDhwI76-e4WHMoCSPZsUzD2npHbnLCqnX4",
+  authDomain: "autohub-dev.firebaseapp.com",
+  projectId: "autohub-dev",
+  storageBucket: "autohub-dev.firebasestorage.app",
+  messagingSenderId: "595518888404",
+  appId: "1:595518888404:web:4add5ec9c3348bc7da5e41",
+  measurementId: "G-TE884G6ZTX",
 } as const;
 
 // 클라이언트 환경에서 Firebase 앱 초기화 (중복 초기화 방지)
